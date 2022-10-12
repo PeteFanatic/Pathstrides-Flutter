cdb3755faad2f657ec0d605c518343a8c9681107
Maegan <maegan.parales@usjr.edu.ph>
2022-10-13 01:23:21 +0800

    Added app bar

--- lib/Screens/pointsshop_screen.dart
+++ lib/Screens/pointsshop_screen.dart
@@ -6,6 +6,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
+import 'home_screen.dart';
+
 class PointsShopScreen extends StatefulWidget {
   const PointsShopScreen({super.key});
 
@@ -17,6 +19,41 @@ class _PointsShopScreenState extends State<PointsShopScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: const Color.fromARGB(255, 255, 126, 45),
+          ),
+          onPressed: () {
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const HomeScreen()));
+          },
+        ),
+        title: Text(
+          'Points Shop',
+          style: TextStyle(
+            color: Color.fromARGB(255, 255, 153, 0),
+            fontFamily: 'Inter-Bold',
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        toolbarHeight: 120,
+        flexibleSpace: Container(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            child: Text(
+              'Points: 42,069',
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 153, 0),
+                fontFamily: 'Inter-Bold',
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ),
+      ),
       backgroundColor: Colors.white,
       bottomNavigationBar: Container(
         color: Colors.white,
