3a3641a7ab0e5d88a5b06ea678beebeec8e8af75
Maegan <maegan.parales@usjr.edu.ph>
2022-10-24 12:29:18 +0800

    Added AppBar and Badge Dependency

--- lib/Screens/pointsshop_screen.dart
+++ lib/Screens/pointsshop_screen.dart
@@ -6,6 +6,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
+import '../widgets/HomeAppBar.dart';
+
 class PointsShopScreen extends StatefulWidget {
   const PointsShopScreen({super.key});
 
@@ -17,28 +19,31 @@ class _PointsShopScreenState extends State<PointsShopScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      bottomNavigationBar: Container(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
-          child: GNav(
-            gap: 8,
-            padding: EdgeInsets.all(15),
-            backgroundColor: Colors.white,
-            color: Color.fromARGB(255, 255, 153, 0),
-            activeColor: Color.fromARGB(255, 255, 153, 0),
-            tabBackgroundColor: Color.fromARGB(69, 255, 153, 0),
-            tabs: const [
-              GButton(icon: Icons.home),
-              GButton(icon: Icons.announcement),
-              GButton(icon: Icons.list),
-              GButton(icon: Icons.notifications),
-              GButton(icon: Icons.person),
-            ],
-          ),
-        ),
-      ),
+      // backgroundColor: Colors.white,
+      // bottomNavigationBar: Container(
+      //   color: Colors.white,
+      //   child: Padding(
+      //     padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+      //     child: GNav(
+      //       gap: 8,
+      //       padding: EdgeInsets.all(15),
+      //       backgroundColor: Colors.white,
+      //       color: Color.fromARGB(255, 255, 153, 0),
+      //       activeColor: Color.fromARGB(255, 255, 153, 0),
+      //       tabBackgroundColor: Color.fromARGB(69, 255, 153, 0),
+      //       tabs: const [
+      //         GButton(icon: Icons.home),
+      //         GButton(icon: Icons.announcement),
+      //         GButton(icon: Icons.list),
+      //         GButton(icon: Icons.notifications),
+      //         GButton(icon: Icons.person),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: ListView(children: [
+        HomeAppBar(),
+      ]),
     );
   }
 }
