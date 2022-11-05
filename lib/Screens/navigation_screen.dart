77ea519af42ab4ffe196118e2d0a8b760c60aaa7
Maegan <maegan.parales@usjr.edu.ph>
2022-11-06 01:06:58 +0800

    added imports and created NagicationScreen class

---
+++ lib/Screens/navigation_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:location/location.dart' as loc;
+import 'package:location/location.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:math' show cos, sqrt, asin;
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
