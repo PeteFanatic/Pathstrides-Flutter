67260a6efb7b25e02d54eb8f4a721c1e3ab5e716
Maegan <maegan.parales@usjr.edu.ph>
2022-12-13 04:02:15 +0800

    shop.dart

---
+++ lib/Models/shop.dart
@@ -0,0 +1,12 @@
+class ShopData {
+  List<ShopItem> shopitems;
+
+  ShopData({this.shopitems});
+}
+
+class ShopItem {
+  String points_name;
+  int points;
+
+  ShopItem({this.points_name, this.points});
+}
