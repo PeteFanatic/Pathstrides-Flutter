744ee15ee1965669f891414552164223bb8d1095
Maegan <maegan.parales@usjr.edu.ph>
2022-12-30 01:57:54 +0800

    edited product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,28 +1,21 @@
 class Product {
-  int? id;
-  String? title;
-  num? price;
-  String? category;
-  String? description;
-  String? image;
+  // int? id;
+  // String? title;
+  // num? price;
+  // String? category;
+  // String? description;
+  // String? image;
 
-  Product({
-    this.id,
-    this.title,
-    this.price,
-    this.category,
-    this.description,
-    this.image,
-  });
+  int points = 0;
+  int points_id = 0;
+  int user_id = 0;
+  String points_name = "";
 
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-      id: json['id'],
-      title: json['title'],
-      price: json['price'],
-      category: json['category'],
-      description: json['description'],
-      image: json['image'],
-    );
-  }
+  Product(this.points, this.points_id, this.user_id, this.points_name);
+
+  Product.fromJson(Map json)
+      : points_id = json['points_id'],
+        points_name = json['points_name'],
+        points = json['points'],
+        user_id = json['user_id'];
 }
